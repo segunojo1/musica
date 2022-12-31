0db133cc8f99a61da66797aa5e28ae73826bbd05
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-31 19:18:30 +0100

    hero section(not done)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {CiSearch} from 'react-icons/ci'
+import HeroImg from '../assets/hero1.png'
 
 const Hero = () => {
   return (
     <div className=''>
-        <input type="text" className='p-[2rem] w-full'/>
+        <div className='relative mb-[1rem]'>
+            <CiSearch className='absolute my-auto h-full ml-[2rem]'/>
+            <input type="text" className='p-[1.5rem] px-[4rem]  w-full text-lg' placeholder='       Search artists'/>
+        </div>
+        <div>
+            <img src={HeroImg} alt="hero" />
+            <div>
+                <h1>Top charts</h1>
+            </div>
+        </div>
     </div>
   )
 }
