e61364a3fec32b19fc76638cda66dc0fa7d37ee6
lil-dev16 <olusegunkd2017@gmail.com>
2023-01-01 16:11:31 +0100

    album section

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App bg-bla p-[2rem] pr-[5rem] text-white ">
-      <Home />
-    </div>
+      <div className="App bg-bla p-[2rem] pr-[5rem] text-white ">
+          <Home />
+      </div>
   )
 }
 
--- src/components/ChartsComp.jsx
+++ src/components/ChartsComp.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import Heart from '../assets/Heart.png'
 
 const ChartsComp = ({img, title, name, timestamp}) => {
   return (
-    <div className='bg-dark p-[1rem] flex gap-[1rem] rounded-lg w-[29rem]'>
-        <img src={img} alt="" />
-        <div>
-            <p className='text-lg'>{title}</p>
-            <p className='text-sm text-[#383833]'>{name}</p>
-            <p>{timestamp}</p>
+    <div className='bg-dark p-[1rem] flex items-center justify-between rounded-lg w-[29rem]'>
+        <div className='flex gap-[2rem]'>
+            <img src={img} alt="" />
+            <div>
+                <p className='text-lg'>{title}</p>
+                <p className='text-sm text-[#383833]'>{name}</p>
+                <p>{timestamp}</p>
+            </div>
         </div>
+        <img src={Heart} alt="like it!" className='p-[1rem] border rounded-full' />
     </div>
   )
 }
